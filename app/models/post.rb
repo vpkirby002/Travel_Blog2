2a769d1f013da54cc6fbd35eefad46a0da06051e
vpkirby002 <vpkirby002@gmail.com>
2017-10-29 15:55:59 -0400

    take2

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,7 @@
 class Post < ApplicationRecord
 	belongs_to :user
 	has_many :comments
+	
 	 has_attached_file :image, styles: { large: "600x600>", medium: "300x300>", thumb: "150x150#"}
 	 validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 end
